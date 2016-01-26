1bb1d78829991c4a993b98eb1e6fe44bc92a89cf
hecomwufenyao <wufenyao@hecom.cn>
2016-01-26 16:49:46 +0800

    init commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT"); 
+header("Cache-Control: no-cache, must-revalidate"); 
+header("Pramga: no-cache"); 
+
+function is_weixin(){ 
+    if ( strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false ) {
+        return true;
+    }
+    return false;
+}
+
+function get_device_type() {
+    $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+    $type = 'other';
+    if (strpos($agent, 'iphone') || strpos($agent, 'ipad')) {
+        $type = 'ios';
+    }
+    if (strpos($agent, 'android')) {
+        $type = 'android';
+    }
+    return $type;
+}
+
+$iswx = is_weixin();
+
+$devicetype = 'android';
+if (get_device_type() == 'ios'){
+    $devicetype = 'ios';
+}
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>红圈营销</title>
+	<meta http-equiv="pragram" content="no-cache">
+	<style>
+		body{
+			text-align: center; 
+			padding:0px; 
+			margin:0px;
+			line-height:0px;
+			background-color:#a0a5b0;
+		}
+		.hidden{ display:none; z-index:2;}
+	</style>
+		
+</head>
+
+<body>
+<div class="hidden">
+	<img src="<?php echo $devicetype;?>img/a.png" />
+</div>
+      	<img src="<?php echo $devicetype;?>img/top.png" />
+        <?php
+        $url = "";
+        if ($devicetype == 'ios'){
+            if ($iswx){
+//                $url = "itms-services://?action=download-manifest&url=https://v40.hecom.cn/plist/newBiaoZhun4.plist";
+                $url = "itms-services://?action=download-manifest&url=https://v40.hecom.cn/plist/biaoZhun4Release.plist";
+            }else{
+//                $url = "itms-services://?action=download-manifest&url=https://v40.hecom.cn/plist/newBiaoZhun4.plist";
+//                $url = "itms-services://?action=download-manifest&url=https://v40.hecom.cn/plist/biaoZhun4Release.plist";
+                $url = "https://appsto.re/cn/K4TQ5";
+            }
+        }else{
+            if ($iswx){
+//                $url = "http://a.app.qq.com/o/simple.jsp?pkgname=com.hecom.sales";
+                $url = "http://a.app.qq.com/o/simple.jsp?pkgname=com.hecom.sales";
+            }else{
+//                $url = "http://www.sosgps.com.cn/filemanage/android/4.3.0/tsclient.apk";
+                $url = "http://www.sosgps.com.cn/filemanage/android/HecomClientRC.apk";
+            }
+        }
+        ?>
+        <a href="<?php echo $url;?>" target="_top">
+		<img src="<?php echo $devicetype;?>img/btn.png" />
+	</a>
+</body>
+</html>
